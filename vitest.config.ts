1b219c133b8443786f40cfa5ea8b0b393a230a06
rapaglaz <paul@rapaglaz.de>
2025-11-27 00:37:29 +0100

    refactor(tests): add dedicated viktest config

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text-summary', 'lcov', 'html'],
+      thresholds: {
+        statements: 80,
+        branches: 70,
+        functions: 80,
+        lines: 80,
+      },
+    },
+  },
+});
