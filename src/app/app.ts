073a4ced8224108e9879acbb22e61a7e8f325bcb
rapaglaz <paul@rapaglaz.de>
2025-12-21 06:14:27 +0100

    refactor(app): update app structure with navbar and footer
    Add Navbar and Footer components to the app's main template.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer, Navbar } from './features';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `<router-outlet />`,
+  imports: [Navbar, RouterOutlet, Footer],
+  template: `
+    <app-navbar />
+    <router-outlet />
+    <app-footer />
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {}
