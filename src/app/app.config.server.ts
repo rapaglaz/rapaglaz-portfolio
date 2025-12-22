dd287a70baa680d47f0e08bb255864ba7f45fff1
Paul Glaz <paul@rapaglaz.de>
2025-12-22 07:47:01 +0100

    fix(ssr): split router config for server (#103)

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,11 +1,17 @@
 import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
+import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
 import { TRANSLOCO_LOADER } from '@jsverse/transloco';
-import { appConfig } from './app.config';
+import { appConfigBase } from './app.config';
+import { routes } from './app.routes';
 import { TranslocoFsLoader } from './utils/i18n';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering(), { provide: TRANSLOCO_LOADER, useClass: TranslocoFsLoader }],
+  providers: [
+    provideServerRendering(),
+    provideRouter(routes, withEnabledBlockingInitialNavigation()),
+    { provide: TRANSLOCO_LOADER, useClass: TranslocoFsLoader },
+  ],
 };
 
-export const appConfigServer = mergeApplicationConfig(appConfig, serverConfig);
+export const appConfigServer = mergeApplicationConfig(appConfigBase, serverConfig);
